0967407015dd3c053fa8dcc77372094ff4f146de
SohumSKS <37936322+SohumSKS@users.noreply.github.com>
2022-10-31 23:03:37 -0500

    Revert "links"
    This reverts commit 7a5e58afadaf382b88fa4a27c2c6e6da6781384b.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/sohumsuthar"
+                href="https://github.com/SohumSKS"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"

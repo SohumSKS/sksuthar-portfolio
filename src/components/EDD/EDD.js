0387a05b4d9b17f0ac85db1f7c3af32afa5d7340
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-02-16 01:30:45 -0600

    k

--- src/components/EDD/EDD.js
+++ src/components/EDD/EDD.js
@@ -24,7 +24,6 @@ function Projects() {
             Fifty-eight percent of homeowners with a lawn dislike mowing their lawn from a survey of over 1,000 people in 2008. Those who refuse to mow their own lawn spend an average of $45 every mow to keep their lawn trimmed. A majority of homeowners dislike lawn mowing due to the time it takes, the heat on warm days, and the noise from the lawnmower.
             </p>
           </Col>
-          <iframe width = "1250px" height = "500px" src="https://a360.co/3jmJHQy" />;
 
           <Row style={{ justifyContent: "left", paddingBottom: "10px" }}>
             <Col md={4} className="EDD-card">
